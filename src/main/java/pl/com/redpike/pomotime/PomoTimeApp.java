b8f4885bd6b63fed32bd04095f26879111c6e05f
Redpike <rsokulski@op.pl>
2017-09-05 20:50:13 +0200

    Configured JavaFX plugin for Gradle and added HelloWorld class.

---
+++ src/main/java/pl/com/redpike/pomotime/PomoTimeApp.java
@@ -0,0 +1,26 @@
+package pl.com.redpike.pomotime;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import pl.com.redpike.pomotime.config.PomoTimeConfig;
+
+/**
+ * Created by Redpike
+ */
+public class PomoTimeApp extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle(PomoTimeConfig.APP_TITLE);
+        Button button = new Button();
+        button.setText("Say 'Hello World'");
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(button);
+        primaryStage.setScene(new Scene(stackPane, PomoTimeConfig.APP_WIDTH, PomoTimeConfig.APP_HEIGHT));
+        primaryStage.show();
+    }
+}
---
+++ src/main/java/pl/com/redpike/pomotime/config/PomoTimeConfig.java
@@ -0,0 +1,11 @@
+package pl.com.redpike.pomotime.config;
+
+/**
+ * Created by Redpike
+ */
+public class PomoTimeConfig {
+
+    public static final String APP_TITLE = "PomoTime";
+    public static final Double APP_WIDTH = 200d;
+    public static final Double APP_HEIGHT = 150d;
+}
